3b8b5525189a72317c6eca4de9794845c6abe3a6
101arrowz <arjunbarrett@gmail.com>
2019-07-05 10:56:40 -0700

    Made service-worker caching work for everything but conjugations

--- public/sw.js
+++ public/sw.js
@@ -9,14 +9,57 @@ self.addEventListener('install', e => {
       },
     })
       .then(res => res.json())
-      .then(res => res.filter(el => !el.startsWith('sw.js')).map(el => '/' + el))
-      .then(vals => caches.open(CURR_CACHE).then(cache => cache.addAll(vals))),
+      .then(res => res.map(el => '/' + el))
+      .then(vals =>
+        caches
+          .open(CURR_CACHE)
+          .then(cache => {
+            cache.addAll(vals);
+            return cache;
+          })
+          .then(cache => {
+            fetch(info.SERVER_URL + '/suggestAll', {
+              headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+              },
+            })
+              .then(res => res.json())
+              .then(val =>
+                cache.put(info.SERVER_URL + '/suggestAll', new Response(JSON.stringify(val))),
+              );
+            return cache;
+          })
+          .then(cache => {
+            fetch(info.SERVER_URL + '/popularity', {
+              headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+              },
+            })
+              .then(res => res.json())
+              .then(val =>
+                cache.put(info.SERVER_URL + '/popularity', new Response(JSON.stringify(val))),
+              );
+          }),
+      ),
   );
 });
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(res => {
-      return res ? res : fetch(event.request);
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res
+        ? res
+        : e.request.url.startsWith(info.SERVER_URL)
+        ? fetch(e.request)
+            .then(val =>
+              caches.open(CURR_CACHE).then(cache => {
+                cache.put(e.request.url, val.clone());
+                return val;
+              }),
+            )
+            .catch(err => console.log('ERROR:', err))
+        : caches.match('/index.html');
     }),
   );
 });
