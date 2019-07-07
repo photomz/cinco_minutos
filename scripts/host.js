efe91c8a9a78473abf9f9a73dc940e331ddcf7da
101arrowz <arjunbarrett@gmail.com>
2019-07-06 19:31:34 -0700

    Added host script

---
+++ scripts/host.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+//eslint-disable-next-line
+const distDir = fs.readdirSync(path.join(__dirname, '../dist'));
+let app = express();
+let httpRedirect = express();
+//eslint-disable-next-line
+httpRedirect.get('*', (req, res) => {
+  res.redirect('https://' + req.get('Host') + req.url);
+});
+distDir.map(el =>
+  app.get('/' + el, (req, res) => {
+    // eslint-disable-next-line
+    res.sendFile(path.resolve(__dirname, '../dist/' + el));
+  }),
+);
+
+app.get('*', (req, res) => {
+  // eslint-disable-next-line
+  res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+});
+http.createServer(httpRedirect).listen(80);
+https.createServer(app).listen(443);
